7d00daeed899e154c680b0de7d10ab14f9e5de7e
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-16 20:27:58 +0000

    + dist

---
+++ dist/config.d.ts
@@ -0,0 +1,3 @@
+export declare const model: string;
+export declare const index: string;
+export declare const namespace: string;
---
+++ dist/main.d.ts
@@ -0,0 +1,1 @@
+export default function main(created?: string[], updated?: string[], removed?: string[]): Promise<void>;
