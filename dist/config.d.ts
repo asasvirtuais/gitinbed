73f2f4b04d9afd257bd6c82c053528347b4f9ab0
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-18 10:07:49 +0000

    no dist anymore

--- dist/config.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const model: string;
-export declare const index: string;
-export declare const namespace: string;
--- dist/pinecone/index.js
+++
@@ -1,9 +0,0 @@
-import * as config from '@/config';
-import { Pinecone } from '@pinecone-database/pinecone';
-const pinecone = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
-export const index = pinecone.index(config.index);
-export const namespace = index.namespace(config.namespace);
-export const create = (data) => namespace.upsert(data);
-export const update = (data) => namespace.update(data);
-export const destroy = (ids) => (ids.length === 1 ? namespace.deleteOne(ids[0]) : namespace.deleteMany(ids));
-export default pinecone;
