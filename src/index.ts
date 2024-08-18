da4d7da3de8b0cf0473762d40c2e96e072a1c6db
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-18 10:54:29 +0000

    + silliest logger

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
 #!/usr/bin/env node
-
-import { config } from 'dotenv'
+import './logger'
+import 'dotenv/config'
 import main from './main'
-
-// Load environment variables from .env file
-config()
-
-// Ensure required environment variables are set (thanks Claude)
-const requiredEnvVars = ['VOYAGE_MODEL', 'PINECONE_INDEX', 'PINECONE_NAMESPACE', 'PINECONE_API_KEY', 'VOYAGE_API_KEY']
-for (const envVar of requiredEnvVars) {
-	if (!process.env[envVar]) {
-		console.error(`Error: ${envVar} is not set in the environment`)
-		process.exit(1)
-	}
-}
 
 // Process command-line arguments
 const args = process.argv.slice(2)
---
+++ src/logger.ts
@@ -0,0 +1,24 @@
+import { Logger, ILogObj } from 'tslog'
+
+const debug = process.argv.slice(2).find(arg => arg.startsWith('--debug')) || process.env.NODE_ENV !== 'production'
+
+class CustomLogger extends Logger<ILogObj> {
+  constructor(settings: any) {
+    super(settings)
+  }
+
+  sillier<T>(whatever: T, ...args: any[]): T {
+    super.silly(whatever, ...args)
+    return whatever
+  }
+}
+
+const logger = new CustomLogger({ minLevel: debug ? 0 : 3 })
+
+declare global {
+  var logger: CustomLogger
+}
+
+global.logger = logger
+
+export default logger
