a77e2853649701625f67a7c1ac14e804bcc01da9
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-08-18 11:21:30 +0000

    Delete all

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-
-export const model = process.env.VOYAGE_MODEL as string
-export const index = process.env.PINECONE_INDEX as string
-export const namespace = process.env.PINECONE_NAMESPACE as string
